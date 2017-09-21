bd06d33a956e927ae3930f5eb02c405f87cc8dc4
Lee Nguyen <lee.nguyen@innovatube.com>
2017-09-22 01:26:59 +0700

    fuck bitch

--- src/redux/gifs/epic.js
+++ src/redux/gifs/epic.js
@@ -2,14 +2,16 @@ import { actions, types } from './actions'
 import 'rxjs'
 import { Observable } from 'rxjs/Observable'
 
-const API_URL = 'http://api.giphy.com/v1/gifs/search?q='
+const API_URL = 'https://api.giphy.com/v1/gifs/search?q='
 const API_KEY = '&api_key=dc6zaTOxFJmzC'
 
 const requestGifsEpic = action$ =>
-  action$.ofType(types.REQUEST_GIFS)
+  action$
+    .ofType(types.REQUEST_GIFS)
     .mergeMap(action =>
-      Observable.ajax(`${API_URL}${action.term.replace(/\s/g, '+')}${API_KEY}`)
-        .map(res => actions.requestGifsDone(res.response.data))
+      Observable.ajax(
+        `${API_URL}${action.term.replace(/\s/g, '+')}${API_KEY}`
+      ).map(res => actions.requestGifsDone(res.response.data))
     )
 
 export default requestGifsEpic
